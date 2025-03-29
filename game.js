21fc3b9eee0bb333505135dbc4c9328def7a0213
Dawsyn <48812637+Dawsyn@users.noreply.github.com>
2025-03-29 12:30:17 -0500

    added enemy movement file
    Moved methods from game.js to enemyMovement.js

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 import * as Movement from "./src/controllers/characterMovement.js";
 import * as GhostBehavior from "./src/controllers/ghostBehaviors.js";
 import * as characterDeath from "./src/controllers/characterDeath.js";
+import * as EnemyMovement from "./src/controllers/enemyMovement.js";
 
 
 class Pacman extends Phaser.Scene {
@@ -187,7 +188,7 @@ class Pacman extends Phaser.Scene {
     this.physics.add.overlap(this.pacman,this.powerPills,this.eatPowerPill,null,this);
     this.cursors = this.input.keyboard.createCursorKeys();
     this.detectIntersections();
-    this.initializeGhosts(layer);
+    EnemyMovement.initializeGhosts.call(this, layer);
     let startPoint = {x:232,y:240};
     this.pinkGhost.path = GhostBehavior.aStarAlgorithm.call(this, startPoint,this.PINKY_SCATTER_TARGET);
     this.pinkGhost.nextIntersection = this.pinkGhost.path.shift();
@@ -215,43 +216,6 @@ class Pacman extends Phaser.Scene {
       this.scene.pause();              // Pause the current scene
     });
     }
-
-  initializeGhosts(layer) {
-    this.pinkGhost = this.initializeGhost(232,290,"pinkGhost",layer);
-    this.orangeGhost = this.initializeGhost(210,290,"orangeGhost",layer);
-    this.redGhost = this.initializeGhost(232,290,"redGhost",layer);
-    this.blueGhost = this.initializeGhost(255,290,"blueGhost",layer);
-    this.ghosts = [this.pinkGhost,this.redGhost,this.orangeGhost,this.blueGhost];
-    this.startGhostEntries();
-  }
-
-  startGhostEntries() {
-    this.ghosts.forEach((ghost,index)=>{
-      if (ghost.entryTimer) {
-        clearTimeout(ghost.entryTimer);
-      }
-      ghost.entryTimer =  setTimeout(()=>{
-        this.enterMaze(ghost);
-      },this.entryDelay*(index));
-    });
-  }
-
-  enterMaze(ghost) {
-    ghost.setPosition(232,240);
-    ghost.enteredMaze = true;
-    if(this.currentMode !== "scared")
-      ghost.hasBeenEaten = true;
-  }
-  initializeGhost(x,y,spriteKey,layer) {
-    const ghost = this.physics.add.sprite(x,y,spriteKey);
-    this.physics.add.collider(ghost,layer);
-    ghost.originalTexture = spriteKey;
-    ghost.direction = "right";
-    ghost.previousDirection = "right";
-    ghost.nextIntersection = null;
-    ghost.enteredMaze = false;
-    return ghost;
-  }
 
 
   populateBoardAndTrackEmptyTiles(layer) {
@@ -388,7 +352,7 @@ class Pacman extends Phaser.Scene {
     GhostBehavior.updateGhostPath.call(this, ghost,target);
     ghost.direction = "left";
   });
-  this.startGhostEntries();
+  EnemyMovement.startGhostEntries.call(this);
   GhostBehavior.setModeTimer.call(this, this.scatterModeDuration);
   this.currentMode = "scatter";
   this.previouseMode = this.currentMode;
@@ -400,7 +364,7 @@ respawnGhost(ghost) {
   ghost.setVisible(true);
   ghost.setTexture(ghost.originalTexture);
   ghost.hasBeenEaten = true;
-  this.enterMaze(ghost);
+  EnemyMovement.enterMaze.call(this, ghost);
   let target = this.currentMode === "chase" ?
   GhostBehavior.getChaseTarget.call(this, ghost) : GhostBehavior.getScatterTarget.call(this, ghost);
   GhostBehavior.updateGhostPath.call(this, ghost,target);
@@ -415,174 +379,24 @@ respawnGhost(ghost) {
     Movement.teleportPacmanAcrossWorldBounds.call(this);
 
     if(this.pinkGhost.enteredMaze) {
-     this.handleGhostDirection(this.pinkGhost);
-     this.handleGhostMovement(this.pinkGhost);
+     EnemyMovement.handleGhostDirection.call(this, this.pinkGhost);
+     EnemyMovement.handleGhostMovement.call(this, this.pinkGhost);
     }
     if(this.orangeGhost.enteredMaze) {
-     this.handleGhostDirection(this.orangeGhost);
-     this.handleGhostMovement(this.orangeGhost);
+     EnemyMovement.handleGhostDirection.call(this, this.orangeGhost);
+     EnemyMovement.handleGhostMovement.call(this, this.orangeGhost);
     }
     if(this.blueGhost.enteredMaze) {
-      this.handleGhostDirection(this.blueGhost);
-      this.handleGhostMovement(this.blueGhost);
+      EnemyMovement.handleGhostDirection.call(this, this.blueGhost);
+      EnemyMovement.handleGhostMovement.call(this, this.blueGhost);
     }
     if(this.redGhost.enteredMaze) {
-      this.handleGhostDirection(this.redGhost);
-      this.handleGhostMovement(this.redGhost);
+      EnemyMovement.handleGhostDirection.call(this, this.redGhost);
+      EnemyMovement.handleGhostMovement.call(this, this.redGhost);
     }
   }
     
 
-  handleGhostDirection(ghost) {
-    if(GhostBehavior.isInghostHouse.call(this, ghost.x,ghost.y)){
-      this.changeGhostDirection(ghost,0,-this.ghostSpeed);
-      if(ghost.direction === "down")
-      ghost.direction = "up";
-    }
-
-    const isMoving = ghost.body.velocity.x !== 0 || ghost.body.velocity.y !== 0;
-    if(!isMoving) {
-      ghost.stuckTimer = (ghost.stuckTimer || 0) +1;
-      if(ghost.stuckTimer>30) {
-        ghost.stuckTimer = 0;
-        let newTarget = this.currentMode === "scared" ? GhostBehavior.getScaredTarget.call(this) :
-        this.currentMode === "chase" ? GhostBehavior.getChaseTarget.call(this, ghost) : GhostBehavior.getScatterTarget.call(this, ghost);
-        GhostBehavior.updateGhostPath.call(this, ghost,newTarget);
-      }
-    } else 
-        ghost.stuckTimer = 0;
-
-    if(ghost.body.velocity.x==0 && ghost.body.velocity.y == 0) {
-      this.adjustGhostPosition(ghost);
-    }
-
-    let isAtIntersection = this.isGhostAtIntersection(ghost.nextIntersection,ghost.x,ghost.y,ghost.direction);
-  
-    if(isAtIntersection) {
-      if((this.PINKY_SCATTER_TARGET.x === ghost.nextIntersection.x) && (this.PINKY_SCATTER_TARGET.y === ghost.nextIntersection.y) && this.currentMode === "scatter" && ghost.texture.key==="pinkGhost")
-        return;
-      if((this.BLINKY_SCATTER_TARGET.x === ghost.nextIntersection.x) && (this.BLINKY_SCATTER_TARGET.y === ghost.nextIntersection.y) && this.currentMode === "scatter" &&  ghost.texture.key==="redGhost")
-        return;
-      if((this.INKY_SCATTER_TARGET.x === ghost.nextIntersection.x) && (this.INKY_SCATTER_TARGET.y === ghost.nextIntersection.y) && this.currentMode === "scatter" &&  ghost.texture.key==="blueGhost")
-        return;
-      if((this.CLYDE_SCATTER_TARGET.x === ghost.nextIntersection.x) && (this.CLYDE_SCATTER_TARGET.y === ghost.nextIntersection.y) && this.currentMode === "scatter" &&  ghost.texture.key==="orangeGhost")
-        return;
-
-      if(this.currentMode === "chase") {
-        let chaseTarget = GhostBehavior.getChaseTarget.call(this, ghost);
-        GhostBehavior.updateGhostPath.call(this, ghost,chaseTarget);
-      }
-
-      if(ghost.path.length>0) {
-        ghost.nextIntersection = ghost.path.shift();
-      }
-      if(ghost.path.length ==0 && this.currentMode === "scared") {
-        let scaredTarget = GhostBehavior.getScaredTarget.call(this);
-        GhostBehavior.updateGhostPath.call(this, ghost,scaredTarget);
-      }
-      
-      let newDirection = this.getGhostNextDirection(ghost,ghost.nextIntersection);
-      ghost.previousDirection = ghost.direction;
-      ghost.direction = newDirection;
-    }
-  }
-
- adjustGhostPosition(ghost) {
-  if(ghost.x%this.blockSize!==0){
-    let nearestMultiple = Math.round(ghost.x/this.blockSize)*this.blockSize;
-    ghost.body.reset(nearestMultiple,ghost.y);
-  }  
-  if(ghost.y%this.blockSize!==0){
-    let nearestMultiple = Math.round(ghost.y/this.blockSize)*this.blockSize;
-    ghost.body.reset(ghost.x,nearestMultiple);
-  }
-
- }
- 
- isGhostAtIntersection(intersection,currentX,currentY,direction) {
-  const isUp = direction === "up";
-  const isDown = direction === "down";
-  const isLeft = direction === "left";
-  const isRight = direction === "right";
-   
-  let condition = ((isUp && intersection.x === currentX && intersection.y>=currentY) ||(isDown && intersection.x === currentX && intersection.y<=currentY) || (isLeft && intersection.y === currentY && intersection.x>=currentX) || (isRight && intersection.y === currentY && intersection.x<=currentX) );
-  return condition;
- }
- 
- getGhostNextDirection(ghost,intersection) {
-  if(Math.abs(intersection.x-ghost.x)<this.blockSize && ghost.y<=intersection.y)
-    return "down";
-  if(Math.abs(intersection.x-ghost.x)<this.blockSize && ghost.y>=intersection.y)
-    return "up";
-  if(Math.abs(intersection.y-ghost.y)<this.blockSize && ghost.x<=intersection.x)
-    return "right";
-  if(Math.abs(intersection.y-ghost.y)<this.blockSize && ghost.x>=intersection.x)
-    return "left";
-  return "up";
- }
-
-
-  handleGhostMovement(ghost) {
-    let nextIntersectionx = null;
-    let nextIntersectiony = null;
-    if(ghost.nextIntersection) {
-      nextIntersectionx = ghost.nextIntersection.x;
-      nextIntersectiony = ghost.nextIntersection.y;
-    }
-    switch(ghost.direction) {
-      case "left":
-        this.handleGhostMovementInDirection(ghost,"left","right",ghost.y,nextIntersectiony,ghost.x,-this.ghostSpeed,0,ghost.body.velocity.y);
-        break;
-        case "right":
-          this.handleGhostMovementInDirection(ghost,"right","left",ghost.y,nextIntersectiony,ghost.x,this.ghostSpeed,0,ghost.body.velocity.y);
-          break; 
-       case "up":
-          this.handleGhostMovementInDirection(ghost,"up","down",ghost.x,nextIntersectionx,ghost.y,0,-this.ghostSpeed,ghost.body.velocity.x);
-          break; 
-      case "down":
-        this.handleGhostMovementInDirection(ghost,"down","up",ghost.x,nextIntersectionx,ghost.y,0,this.ghostSpeed,ghost.body.velocity.x);
-        break;      
-    }
-  }
-
-  handleGhostMovementInDirection(ghost,currentDirection,oppositeDirection,ghostPosition,intersectionPosition,movingCoordinate,velocityX,velocityY,currentVelocity) {
-    let perpendicularDirection = currentDirection === "left" || currentDirection === "right" ? ["up","down"]:["left","right"];
-    let condition = false;
-    if(ghost.nextIntersection)
-      condition = (ghost.previousDirection == perpendicularDirection[0] && ghostPosition<=intersectionPosition) || (ghost.previousDirection == perpendicularDirection[1] && ghostPosition>=intersectionPosition) ||(ghost.previousDirection === oppositeDirection);
-    if(condition) {
-      let newPosition = intersectionPosition;
-      if(ghost.previousDirection != oppositeDirection && newPosition !== ghostPosition) {
-        if(currentDirection === "left" || currentDirection === "right")
-          ghost.body.reset(movingCoordinate,newPosition);
-        else ghost.body.reset(newPosition,movingCoordinate);
-      }
-      this.changeGhostDirection(ghost,velocityX,velocityY);
-    }
-    else if (currentVelocity === 0) {
-      this.changeGhostDirection(ghost,velocityX,velocityY);
-    }
-  }
-
-changeGhostDirection(ghost,velocityX,velocityY) {
-  ghost.setVelocityY(velocityY);
-  ghost.setVelocityX(velocityX);
-}
-
-getOppositeDirection(direction) {
-  switch(direction) {
-    case "up":
-      return "down";
-    case "down":
-      return "up";
-    case "left":
-      return "right";
-    case "right":
-      return "left";  
-    default:
-      return "";
-  }
-}
 getPerpendicularDirection(direction) {
   switch(direction) {
     case "up":
